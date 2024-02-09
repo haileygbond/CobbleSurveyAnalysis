b2b8ed3405b7fd27fb30d09399b289f83f59c19b
Hailey Bond <bondh@oregonstate.edu>
2024-02-08 16:11:35 -0800

    improved get_cobble_slopes section

--- calculate_avg_slopes.py
+++ calculate_avg_slopes.py
@@ -5,17 +5,20 @@ from glob import glob
 import os
 from datetime import date
 
-%matplotlib qt5
-
-input_filepath ='C:\\Users\\bondh\Box\\HaileyBond\\phd\\arch cape\\surveys\\4_analysis\\'
+input_filepath ='C:\\Users\\bondh\Box\\HaileyBond\\phd\\arch cape\\surveys\\4_analysis\\cobble_slopes\\'
 
 files = []
-for file in glob(survey_filepath + '*.csv'):
+for file in glob(input_filepath + '*.csv'):
     files.append(file)
 filenames = pd.Series(files)
 
-for file in filenames
-    pd.read_csv(file, columns = ['date', 'transect_name', 'slope'])
-    transects = data_in['feature'].unique()
+data_out = []
+for file in filenames:
+    data_in = pd.read_csv(file, names = ['date', 'transect_name', 'slope'], header=1)
+    transects = data_in['transect_name'].unique()
     for transect in transects:
-        data = data = data_in.loc[data_in['transect_name'] == transect]
+        data = data_in.loc[data_in['transect_name'] == transect]
+        avg_slope = data['slope'].mean()
+        std_dev = data['slope'].std()
+        data_out.append([transect, avg_slope, std_dev])
+        
--- get_survey_params.py
+++ get_survey_params.py
@@ -6,17 +6,21 @@ from glob import glob
 import os
 from datetime import date
 
-survey_filepath = 'C:\\Users\\bondh\\Box\\Dynamic_Revetments_OSG_2022\\SurveyData\\3Processed\\Westport\\'
-output_filepath = 'C:\\Users\\bondh\\Box\\HaileyBond\\phd\\arch cape\\surveys\\4_analysis\\'
-output_filename = 'Westport_cobble_slopes.csv'
+survey_filepath = 'C:\\Users\\bondh\\Box\\Dynamic_Revetments_OSG_2022\\SurveyData\\3Processed\\FC\\'
+output_filepath = 'C:\\Users\\bondh\\Box\\HaileyBond\\phd\\arch cape\\surveys\\4_analysis\\cobble_slopes\\'
+output_filename_prefix = 'FC'
 dates = [20231219]
-transects = ['WORM100', '85', '84.8']
+transects = ['FC1', 'FC2', 'FC3']
 use_all_dates = True
 use_all_transects = False
 
-get_beach_slope = True
+get_beach_slope = False
 beach_slope_method = 'feature_code'
 feature_code = 'rev_toe'
+
+get_cobble_slope = True
+cobble_slope_method = 'feature_code'
+feature_code = 'cobble_toe'
 
 #%%
 if use_all_dates:
@@ -33,12 +37,11 @@ elif len(dates)>1:
 else:
     file = glob(survey_filepath + '*' + str(dates[0]) + '.csv')
     survey_filenames = pd.Series(os.path.basename(file[0])) # this might not work, check!
-#%%
-data_out = []
-for survey_filename in survey_filenames:
-    data_in = pd.read_csv(survey_filepath + survey_filename, header = 0,  names = ['x','y', 'z', 'dist', 'offset', 'feature', 'transect_name'])
-    print(data_in)
-    if get_beach_slope:
+#%% beach slope calculation
+if get_beach_slope:
+    data_out = []
+    for survey_filename in survey_filenames:
+        data_in = pd.read_csv(survey_filepath + survey_filename, header = 0,  names = ['x','y', 'z', 'dist', 'offset', 'feature', 'transect_name'])
         if use_all_transects:
             transects = data_in['feature'].unique()
         for transect in transects:
@@ -60,7 +63,34 @@ for survey_filename in survey_filenames:
                     continue
             else:
                 continue
-            
-# %%
-data_out = pd.DataFrame(data_out, columns = ['date', 'transect_name', 'beach_slope'])
-data_out.to_csv(output_filepath + output_filename, index=False)
+    data_out = pd.DataFrame(data_out, columns = ['date', 'transect_name', 'beach_slope'])
+    data_out.to_csv(output_filepath + output_filename_prefix + '_beach_slopes.csv', index=False)
+#%% cobble slope slope calculation
+if get_cobble_slope:
+    data_out = []
+    for survey_filename in survey_filenames:
+        data_in = pd.read_csv(survey_filepath + survey_filename, header = 0,  names = ['x','y', 'z', 'dist', 'offset', 'feature', 'transect_name'])
+        if use_all_transects:
+            transects = data_in['feature'].unique()
+        for transect in transects:
+            data = data_in.loc[data_in['transect_name'] == transect]
+            if data['feature'].str.contains(feature_code, na=False).any():
+                feature_dist = data[data['feature'].str.contains(feature_code, na=False)]['dist'].iloc[0]
+                if len(data[data['dist']<feature_dist])>0:
+                    beach = data[data['dist']>feature_dist]
+                    beach = beach.reset_index()
+                    beach.drop([0], axis=0, inplace=True) # temp fix for now to get rid of start point
+                    beach = beach.dropna(subset = ['dist', 'z'])
+                    if len(beach)>1:
+                        beach_best_fit = np.polyfit(beach['dist'], beach['z'], 1)
+                        slope = beach_best_fit[0]
+                        surveydate = survey_filename[len(survey_filename)-12:len(survey_filename) - 4]
+                        data_out.append([surveydate, transect, slope])
+                    else:
+                        continue
+                else:
+                    continue
+            else:
+                continue
+    data_out = pd.DataFrame(data_out, columns = ['date', 'transect_name', 'cobble_slope'])
+    data_out.to_csv(output_filepath + output_filename_prefix + '_cobble_slopes.csv', index=False)
